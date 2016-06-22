400b6f9e16026b7c3d1c938fb76ae4f8586403a8
David Lusher <david@Davids-MacBook-Air.local>
2016-06-22 12:33:51 +0100

    fixed

--- matmul.cu
+++ matmul.cu
@@ -17,7 +17,7 @@ __global__ void matrixMultiplySimple(float *a, float *b, float *c, int width)
 	float result = 0;
 
 	// do dot product between row of a and column of b
-	for(int i=0, i<width; i++)
+	for(int i=0; i<width; i++)
 	{
 		result += a[row*width+i] * b[i*width+col];
 	}
@@ -53,7 +53,7 @@ __global__ void matrixMultiplyOptimised(float *a, float *b, float*c, int width)
 		// to continue
 		__syncthreads();
 		// do dot product between row of s_a and column of s_b
-		for(int i=0;, i<TILE_WIDTH; i++)
+		for(int i=0; i<TILE_WIDTH; i++)
 		{
 			result += s_a[ty][i] * s_b[i][tx];
 		}
